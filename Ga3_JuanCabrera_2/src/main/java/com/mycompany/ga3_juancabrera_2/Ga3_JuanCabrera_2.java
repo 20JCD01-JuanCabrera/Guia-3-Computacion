3fbef50ab773aa9931b553f615294ff3bbaee3a1
20JCD01-JuanCabrera <p.cabrera.5bdb@gmail.com>
2026-04-15 20:53:01 -0600

    Add files via upload

---
+++ Ga3_JuanCabrera_2/src/main/java/com/mycompany/ga3_juancabrera_2/Ga3_JuanCabrera_2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ga3_juancabrera_2;
+
+/**
+ *
+ * @author Juan Pablo
+ */
+public class Ga3_JuanCabrera_2 {
+
+    public static void main(String[] args) {
+        jfrmPrincipal hola = new jfrmPrincipal();
+        hola.setVisible(true);
+    }
+}
